a51a3a272288239bedea23da121031b0c3a4511c
Gaurav Kumar <gaurav.kumar@adidas.com>
2022-09-22 01:31:21 +0530

    producer configuration

--- src/main/java/com/example/kafkaProject/KafkaProjectApplication.java
+++ src/main/java/com/example/kafkaProject/KafkaProjectApplication.java
@@ -1,7 +1,10 @@
 package com.example.kafkaProject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaProjectApplication {
@@ -9,5 +12,10 @@ public class KafkaProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaProjectApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String,String> kafkaTemplate){
+		return args->{
+			kafkaTemplate.send("demo-topic","first message");
+		};
+	}
 }
